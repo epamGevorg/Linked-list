670aa7d650c50d934c6190cfcf3690547b74b7a7
epamGevorg <gevorg_martirosyan@epam.com>
2022-04-29 13:14:42 +0400

    easy and shor method of linked list

--- index.js
+++ index.js
@@ -1,149 +1,47 @@
-// User defined class node
-class Node {
-    // constructor
-    constructor(element)
-    {
-        this.element = element;
+class ListNode {
+    constructor(data) {
+        this.data = data;
         this.next = null
     }
 }
 
-// linkedlist class
 class LinkedList {
-    constructor()
-    {
-        this.head = null;
-        this.size = 0;
+    constructor(head = null) {
+        this.head = head
     }
 
-    // functions to be implemented
-    // add(element)
-    add(element) {
-        const node = new Node();
-        let current;
-
-        if (this.head == null) {
-            this.head = node
-        } else {
-            current = this.head;
-
-            while (current.next) {
-                current = current.next
-            }
-
-            current.next = node;
-        }
-        this.size++
-    }
-
-    // insertAt(element, location)
-
-    insertAt(element, index) {
-        if (index < 0 || index > this.size) {
-            return "Please enter a valid index."
-        } else {
-            // creates a new node
-            const node = new Node(element);
-            let curr, prev;
-
-            curr = this.head;
-
-            // add the element to the first index
-            if(index === 0) {
-                node.next = this.head;
-                this.head = node;
-            } else {
-                curr = this.head;
-                let it = 0;
-
-                while (it < index) {
-                    it++;
-                    prev = curr;
-                    curr = curr.next;
-                }
-
-                // adding an element
-                node.next = curr;
-                prev.next = node;
-            }
-            this.size++;
-        }
-    }
-    // removeFrom(location)
-    removeFrom(index) {
-        if(index < 0 || index >= this.size) {
-            return "Please enter a valid index.";
-        } else {
-            let curr, prev, it = 0;
-            curr = this.head;
-            prev = curr;
-
-            if(index === 0) {
-                this.head = curr.next;
-            } else {
-                while (it < index) {
-                    it++;
-                    prev = curr;
-                    curr = curr.next;
-                }
-
-                prev.next = curr.next;
-            }
-            this.size--;
-
-            return curr.element;
-        }
-    }
-    // removeElement(element)
-    removeElement(element) {
-        let current = this.head;
-        let prev = null;
-
-        while (current !== null) {
-            if(current.element === element) {
-                if(prev == null) {
-                    this.head = current.next;
-                } else {
-                    prev.next = current.next;
-                }
-                this.size--;
-                return current.element;
-            }
-            prev = current;
-            current = current.next;
-        }
-        return -1;
-    }
-    // Helper Methods
-    indexOf(element) {
+    size() {
         let count = 0;
-        let current = this.head;
-
-        while (current !== null) {
-            if(current.element === element) {
-                return count;
-            }
+        let node = this.head;
+        while (node) {
             count++;
-            current = current.next;
+            node = node.next
         }
-        return -1;
+        return count;
     }
 
-    idEmpty() {
-        return this.size === 0;
+    clear() {
+        this.head = null;
     }
 
-    size_of_list() {
-        return this.size;
-    }
-
-    printList() {
-        let curr = this.head;
-        let str = '';
-        while(curr) {
-            str += curr.element + " ";
-            curr = curr.next;
+    getLast() {
+        let lastNode = this.head;
+        if(lastNode) {
+            while (lastNode.next) {
+                lastNode = lastNode.next
+            }
         }
-        console.log(str);
+        return lastNode
+    }
+
+    getFirst() {
+        return this.head
     }
 }
+
+const node1 = new ListNode(2);
+const node2 = new ListNode(5);
+
+node1.next = node2
+
+const list = new LinkedList(node1);
